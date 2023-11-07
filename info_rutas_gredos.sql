c3b0e24193261a1dd7492cd13b32103b47b58e5c
MRocioG <rocio.gorgojo@gmail.com>
2023-11-07 11:22:12 +0100

    y había guardado la bbdd en otro sitio... sorry again

---
+++ info_rutas_gredos.sql
@@ -0,0 +1,5 @@
+CREATE SCHEMA rutas_gredos;
+USE rutas_gredos;
+
+LOAD DATA FILE "C:\Users\rocio\OneDrive\Desktop\Data_Analytics\MZ_EJERCICIOS\promo-F-DA-modulo2-evaluacion-intermedia-rociogorgojo\rutas_gredos.csv"
+
